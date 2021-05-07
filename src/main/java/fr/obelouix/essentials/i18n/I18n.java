913128e2dcd472911e8ad4f0bdececad4922f4b0
Louis DELRUE <3874662+obelouix@users.noreply.github.com>
2021-05-07 20:00:41 +0200

    added a method to send translated messages based on player's locale

--- src/main/java/fr/obelouix/essentials/i18n/I18n.java
+++ src/main/java/fr/obelouix/essentials/i18n/I18n.java
@@ -1,6 +1,7 @@
 package fr.obelouix.essentials.i18n;
 
 import fr.obelouix.essentials.Essentials;
+import org.bukkit.entity.Player;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -11,6 +12,7 @@ public class I18n {
     private final String language = Locale.getDefault().getLanguage();
     private final String country = Locale.getDefault().getCountry().toUpperCase(Locale.ROOT);
     private final ResourceBundle messages;
+    private ResourceBundle playerMessages;
 
     private I18n() {
         if (ResourceBundle.getBundle("Messages_" + language + country) == null) {
@@ -27,6 +29,20 @@ public class I18n {
               instance = new I18n();
         }
         return instance;
+    }
+
+    //Method for sending a message based on player's Locale
+    private String translateToPlayerLocale(Player player, String message){
+        final String playerLanguage = player.locale().getLanguage();
+        final String playerCountry = player.locale().getCountry().toUpperCase();
+        if(ResourceBundle.getBundle("Messages_" + playerLanguage + playerCountry) == null){
+            playerMessages = ResourceBundle.getBundle("Messages_" + playerLanguage + playerCountry);
+        }
+        else {
+            //if the plugin doesn't have translations for the player's locale use the default one
+            playerMessages = ResourceBundle.getBundle("Messages_enUS");
+        }
+        return playerMessages.getString(message);
     }
 
     public String getMessage(String message){
