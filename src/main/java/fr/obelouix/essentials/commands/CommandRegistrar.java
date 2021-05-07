a0c9689dc7f345e412076a13caf9bf1a33414f33
Louis DELRUE <3874662+obelouix@users.noreply.github.com>
2021-05-07 22:48:05 +0200

    added method to add translated Aliases to commands

--- src/main/java/fr/obelouix/essentials/commands/CommandRegistrar.java
+++ src/main/java/fr/obelouix/essentials/commands/CommandRegistrar.java
@@ -9,7 +9,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class CommandRegistrar {
 
@@ -19,35 +23,41 @@ public class CommandRegistrar {
     }
 
     public static CommandRegistrar getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new CommandRegistrar();
         }
         return instance;
     }
 
-    public void init(){
-       registerCommand("gamemode", new CommandGameMode());
-       registerCommand("time", new TimeCommand());
-       registerCommand("day", new DayCommand());
-       registerCommand("night", new NightCommand());
-       registerCommand("morning", new MorningCommand());
-       registerCommand("midday", new MiddayCommand());
-       registerCommand("noon", new NoonCommand());
-       registerCommand("midnight", new MidnightCommand());
-       registerCommand("enderchest", new EnderchestCommand());
-       registerCommand("openinv", new OpenInventoryCommand());
-       registerCommand("freeze", new FreezeCommand());
+    public void init() {
+        registerCommand("gamemode", new CommandGameMode());
+        registerCommand("time", new TimeCommand());
+        registerCommand("day", new DayCommand());
+        registerCommand("night", new NightCommand());
+        registerCommand("morning", new MorningCommand());
+        registerCommand("midday", new MiddayCommand());
+        registerCommand("noon", new NoonCommand());
+        registerCommand("midnight", new MidnightCommand());
+        registerCommand("enderchest", new EnderchestCommand());
+        registerCommand("openinv", new OpenInventoryCommand());
+        registerCommand("freeze", new FreezeCommand());
         registerCommand("effect", new EffectCommand());
+        registerCommand("help", new HelpCommand());
+        setAlias("help", I18n.getInstance().setI18NAliases("command.help.alias"));
     }
 
-    private void registerCommand(String command, CommandExecutor executor){
+    private void registerCommand(String command, CommandExecutor executor) {
         Objects.requireNonNull(Essentials.getInstance().getCommand(command)).setExecutor(executor);
         Objects.requireNonNull(Essentials.getInstance().getCommand(command)).setTabCompleter((TabCompleter) executor);
         Objects.requireNonNull(Essentials.getInstance().getCommand(command)).setPermissionMessage(ChatColor.DARK_RED + I18n.getInstance().getMessage("no_permission"));
 
     }
 
-    protected void wrongCommandUsage(CommandSender sender, Command command){
+    private void setAlias(String command, List<String> aliases){
+        Objects.requireNonNull(Objects.requireNonNull(Essentials.getInstance().getCommand(command)).setAliases(aliases));
+    }
+
+    protected void wrongCommandUsage(CommandSender sender, Command command) {
         Player player = (Player) sender;
         player.sendMessage(ChatColor.DARK_RED + I18n.getInstance().getMessage("wrong_command_usage") + "\n" + ChatColor.RED + command.getUsage());
     }
