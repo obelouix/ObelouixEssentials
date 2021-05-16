46a5d34d62979f99f744fe96290048dc755e5818
Louis <3874662+obelouix@users.noreply.github.com>
2021-05-16 13:07:37 +0200

    added ProtocolLib for future uses

--- src/main/java/fr/obelouix/essentials/commands/CommandRegistrar.java
+++ src/main/java/fr/obelouix/essentials/commands/CommandRegistrar.java
@@ -94,7 +94,6 @@ public class CommandRegistrar {
     }
 
     protected void wrongCommandUsage(CommandSender sender, Command command) {
-        // final Player player = (Player) sender;
         sender.sendMessage(ChatColor.DARK_RED + I18n.getInstance().sendTranslatedMessage(sender, "wrong_command_usage") + "\n" + ChatColor.RED + command.getUsage());
     }
 
