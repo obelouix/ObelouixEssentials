f96f466b01baf04914f2badb1e475508ac87462a
Louis DELRUE <3874662+obelouix@users.noreply.github.com>
2021-06-07 12:06:27 +0200

    added the same method but for the console

--- src/main/java/fr/obelouix/essentials/event/CommandPreprocessorEvent.java
+++ src/main/java/fr/obelouix/essentials/event/CommandPreprocessorEvent.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 
 import java.util.Locale;
 
@@ -17,10 +18,20 @@ public class CommandPreprocessorEvent implements Listener {
         final String message = event.getMessage().toLowerCase(Locale.ROOT);
         /* if a player with the permission of reloading the server run this command
            he will be shown this message to ask him to not reload to avoid problems */
-        if (message.startsWith("/reload") && event.getPlayer().hasPermission("bukkit.command.reload")) {
+        if (message.startsWith("reload") && event.getPlayer().hasPermission("bukkit.command.reload")) {
             event.getPlayer().sendMessage(ChatColor.DARK_RED +
                     i18n.sendTranslatedMessage(event.getPlayer(), "avoid_reload"));
         }
     }
 
+    @EventHandler
+    public void preprocessConsoleCommand(ServerCommandEvent event) {
+        final String command = event.getCommand().toLowerCase(Locale.ROOT);
+        /* if the console run the reload command show this
+           message to ask to not reload to avoid problems */
+        if (command.startsWith("reload")) {
+            event.getSender().sendMessage(ChatColor.DARK_RED +
+                    i18n.sendTranslatedMessage(event.getSender(), "avoid_reload"));
+        }
+    }
 }
