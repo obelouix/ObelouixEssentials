b3511aad23a93ddc5d597714c54bf313ff72fd59
Louis <3874662+obelouix@users.noreply.github.com>
2021-06-18 02:24:45 +0200

    enhanced link detection with regex

--- src/main/java/fr/obelouix/essentials/security/ChatSecurity.java
+++ src/main/java/fr/obelouix/essentials/security/ChatSecurity.java
@@ -10,6 +10,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.util.Locale;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class ChatSecurity implements Listener {
 
@@ -20,8 +22,9 @@ public class ChatSecurity implements Listener {
         if (!Config.allowHTTPURL) {
             final Player player = event.getPlayer();
             //force the message tp lowercase and remove all spaces so the player can't bypass url checking
-            final String chatMessage = PlainComponentSerializer.plain().serialize(event.message()).replace(" ", "").toLowerCase(Locale.ROOT);
-            if (chatMessage.contains("http://")) {
+            String chatMessage = PlainComponentSerializer.plain().serialize(event.message()).replace(" ", "").toLowerCase(Locale.ROOT);
+            final Predicate<String> matches = Pattern.compile("^(([a-zA-Z0-9]+)|(http://|ftp://|)(www.|)[a-zA-Z0-9]+(\\.[a-zA-Z]+)+.*)$").asMatchPredicate();
+            if (matches.test(chatMessage)) {
                 player.sendMessage(ChatColor.DARK_RED + i18n.sendTranslatedMessage(player, "chat.security.http.forbidden"));
                 event.setCancelled(true);
             }
