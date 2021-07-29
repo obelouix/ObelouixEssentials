b9c5b653b5cde8a0b77279aebfca1fee8e1e1b42
Louis <3874662+obelouix@users.noreply.github.com>
2021-07-29 19:50:57 +0200

    updated the messages for each events and started to add PlayerInteractEvent

--- src/main/java/fr/obelouix/essentials/event/BlockEvents.java
+++ src/main/java/fr/obelouix/essentials/event/BlockEvents.java
@@ -5,37 +5,59 @@ import fr.obelouix.essentials.i18n.I18n;
 import fr.obelouix.essentials.permissions.IPermission;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class BlockEvents implements Listener {
 
+    /**
+     * This event will fire when a player will try to break a block and cancel the block breaking if he hasn't
+     * the permission for the given block
+     *
+     * @param event
+     */
     @EventHandler
     public void onPlayerBreakBlock(BlockBreakEvent event) {
-        Player player = event.getPlayer();
+        final Player player = event.getPlayer();
         if (!IPermission.canBreak(player, "obelouix.break." + event.getBlock().getTranslationKey())) {
-            player.sendMessage(ChatColor.DARK_RED + I18n.getInstance().sendTranslatedMessage(player, "obelouix.break.disallowed"));
-            @NotNull TranslatableComponent blockComponent = Component.translatable("").key(event.getBlock().getTranslationKey());
-            Essentials.getInstance().getLOGGER().info(player.getName() + " tried to break " + PlainTextComponentSerializer.plainText().serialize(blockComponent));
+            final TranslatableComponent blockComponent = Component.translatable(event.getBlock().getTranslationKey());
+            final Component component = Component.text(I18n.getInstance().sendTranslatedMessage(player, "obelouix.break.disallowed"))
+                    .color(TextColor.color(183, 0, 0))
+                    .append(blockComponent.color(TextColor.color(215, 0, 0)));
+            player.sendMessage(component);
+            Essentials.getInstance().getLOGGER().warning(player.getName() + " tried to break " + PlainTextComponentSerializer.plainText().serialize(blockComponent));
+            event.setCancelled(true);
+        }
+    }
+
+    /**
+     * This event will fire when a player will try to place a block and cancel the block placing if he hasn't
+     * the permission for the given block
+     *
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerPlaceBlock(BlockPlaceEvent event) {
+        final Player player = event.getPlayer();
+        if (!IPermission.canBreak(player, "obelouix.place." + event.getBlock().getTranslationKey())) {
+            final TranslatableComponent blockComponent = Component.translatable(event.getBlock().getTranslationKey());
+            final Component component = Component.text(I18n.getInstance().sendTranslatedMessage(player, "obelouix.place.disallowed"))
+                    .color(TextColor.color(183, 0, 0))
+                    .append(blockComponent.color(TextColor.color(215, 0, 0)));
+            player.sendMessage(component);
+            Essentials.getInstance().getLOGGER().warning(player.getName() + " tried to place " + PlainTextComponentSerializer.plainText().serialize(blockComponent));
             event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerPlaceBlock(BlockPlaceEvent event) {
-        Player player = event.getPlayer();
-        if (!IPermission.canBreak(player, "obelouix.place." + event.getBlock().getTranslationKey())) {
-            player.sendMessage(ChatColor.DARK_RED + I18n.getInstance().sendTranslatedMessage(player, "obelouix.break.disallowed"));
-            @NotNull TranslatableComponent blockComponent = Component.translatable("").key(event.getBlock().getTranslationKey());
-            Essentials.getInstance().getLOGGER().info(player.getName() + " tried to place " + PlainTextComponentSerializer.plainText().serialize(blockComponent));
-            event.setCancelled(true);
-        }
+    public void onPlayerInteract(PlayerInteractEvent event) {
     }
 
 }
