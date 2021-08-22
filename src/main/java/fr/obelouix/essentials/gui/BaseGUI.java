feaac5665e77358c507f30d2e52fe81bf2c9d00d
Louis <3874662+obelouix@users.noreply.github.com>
2021-08-22 18:49:05 +0200

    added documentation

--- src/main/java/fr/obelouix/essentials/gui/BaseGUI.java
+++ src/main/java/fr/obelouix/essentials/gui/BaseGUI.java
@@ -21,14 +21,32 @@ public abstract class BaseGUI implements Listener {
 
     protected final I18n i18n = I18n.getInstance();
 
+    /**
+     * Used to show a custom inventory to a player
+     *
+     * @param player The player who will see the custom inventory
+     */
     public abstract void showInventory(@NotNull Player player);
 
+    /**
+     * Used to cancel the click event to avoid player stealing items in the
+     * custom inventory
+     */
     @EventHandler(priority = EventPriority.NORMAL)
     public abstract void cancelClick(InventoryClickEvent event);
 
+    /**
+     * Control what to do on each clicked objects
+     */
     @EventHandler(priority = EventPriority.NORMAL)
     public abstract void onInventoryClickEvent(InventoryClickEvent event);
 
+    /**
+     * Add a navigation bar at the bottom of an inventory
+     *
+     * @param inventory the inventory were we add the navigation bar
+     * @param player    the player that will see the inventory (needed to translate each objects in the player locale)
+     */
     public void addBottomNavigationBar(@NotNull Inventory inventory, Player player) {
         //previous arrow
         inventory.setItem(48, HeadSkinFetcher.getOfflineSkull(i18n.sendTranslatedMessage(player, "obelouix.previous_page"),
