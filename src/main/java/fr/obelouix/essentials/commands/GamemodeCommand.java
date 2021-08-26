d19553f5ec4cfebd71a46da654857aaf49b54295
Louis <3874662+obelouix@users.noreply.github.com>
2021-08-27 01:27:09 +0200

    fixed little things

--- src/main/java/fr/obelouix/essentials/commands/GamemodeCommand.java
+++ src/main/java/fr/obelouix/essentials/commands/GamemodeCommand.java
@@ -1,13 +1,13 @@
 package fr.obelouix.essentials.commands;
 
 import com.google.common.collect.ImmutableList;
+import fr.obelouix.essentials.Essentials;
 import fr.obelouix.essentials.components.PlayerComponent;
 import fr.obelouix.essentials.i18n.I18n;
 import fr.obelouix.essentials.utils.IPlayer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
@@ -15,7 +15,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -25,19 +24,19 @@ import static org.bukkit.Bukkit.getServer;
 
 public class GamemodeCommand extends BukkitCommand {
 
+    private final Essentials plugin = Essentials.getInstance();
     private final List<String> OPTIONS = ImmutableList.of("survival", "creative", "adventure", "spectator");
     private final I18n i18n = I18n.getInstance();
     private final PlayerComponent playerComponent = new PlayerComponent();
 
     public GamemodeCommand(@NotNull String name) {
         super(name);
+        this.setUsage("/gamemode [<player>|<gamemode>] [<gamemode>]");
     }
-
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         if (args.length > 0 && args.length <= 2) {
-            Component message;
             final Player target = getServer().getPlayer(args[0]);
             if (args.length == 1) {
                 if (isArgumentAPlayer(args[0]) && IPlayer.isOnline(args[0], sender)) {
@@ -57,11 +56,12 @@ public class GamemodeCommand extends BukkitCommand {
                     if (!isArgumentAPlayer(args[0]) && sender instanceof Player player) {
                         changeGamemode(args[0], player);
                         sender.sendMessage(messageOwnGamemodeChanged(sender, args[0]));
-                    }
+                    } else plugin.getLOGGER().info("You can only get or update the gamemode of other player as console"
+                            + "\nLike this: gamemode <player> [gamemode]");
                 }
             } else if (args.length == 2) {
                 if (isArgumentAPlayer(args[0]) && IPlayer.isOnline(args[0], sender)) {
-                    for (String gameMode : OPTIONS) {
+                    for (final String gameMode : OPTIONS) {
                         if (gameMode.equalsIgnoreCase(args[1])) {
                             changeGamemode(gameMode, target);
                             if (Objects.requireNonNull(target).getName().equalsIgnoreCase(sender.getName())) {
@@ -70,7 +70,7 @@ public class GamemodeCommand extends BukkitCommand {
                                 sender.sendMessage(messageTargetGamemodeChanged(sender, target, args[1]));
                             }
                             break;
-                        }
+                        } else CommandManager.wrongCommandUsage(sender, this);
                     }
                 }
 
@@ -89,7 +89,7 @@ public class GamemodeCommand extends BukkitCommand {
     }
 
 
-    private boolean changeGamemode(@NotNull String args, Player player) {
+    private void changeGamemode(@NotNull String args, Player player) {
         if (args.equals("0") || args.equalsIgnoreCase("survival")) {
             player.setGameMode(GameMode.SURVIVAL);
         } else if (args.equals("1") || args.equalsIgnoreCase("creative")) {
@@ -98,10 +98,7 @@ public class GamemodeCommand extends BukkitCommand {
             player.setGameMode(GameMode.ADVENTURE);
         } else if (args.equals("3") || args.equalsIgnoreCase("spectator")) {
             player.setGameMode(GameMode.SPECTATOR);
-        } else {
-            return false;
         }
-        return true;
     }
 
     private Component messageOwnGamemodeChanged(CommandSender sender, String gamemode) {
@@ -129,23 +126,6 @@ public class GamemodeCommand extends BukkitCommand {
                         .build());
     }
 
-    private void updateOtherGameMode(CommandSender sender, Player target, String args) {
-        if (changeGamemode(args, target)) {
-            sender.sendMessage(MessageFormat.format(I18n.getInstance().sendTranslatedMessage(sender, "otherGameModeUpdated"),
-                    ChatColor.AQUA + target.getName() + ChatColor.GREEN,
-                    ChatColor.AQUA + I18n.getInstance().sendTranslatedMessage(sender, target.getGameMode().toString().toLowerCase())));
-            if (target.hasPermission("obelouix.gamemmode.notify")) {
-                target.sendMessage(MessageFormat.format(I18n.getInstance().sendTranslatedMessage(target, "command.gamemode.notify_on_change.author"),
-                        ChatColor.DARK_RED + sender.getName() + ChatColor.GRAY, ChatColor.GREEN + args));
-            } else {
-                target.sendMessage(ChatColor.GRAY + MessageFormat.format(I18n.getInstance().sendTranslatedMessage(target, "command.gamemode.notify_on_change"),
-                        ChatColor.GREEN + args));
-            }
-        } else {
-            // CommandManager.getInstance().wrongCommandUsage(sender, Objects.requireNonNull(getServer().getPluginCommand("gamemode")));
-        }
-    }
-
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
         final List<String> completion = new ArrayList<>();
@@ -161,7 +141,6 @@ public class GamemodeCommand extends BukkitCommand {
             completion.clear();
             StringUtil.copyPartialMatches(args[1], OPTIONS, completion);
         }
-
         return completion;
     }
 }
