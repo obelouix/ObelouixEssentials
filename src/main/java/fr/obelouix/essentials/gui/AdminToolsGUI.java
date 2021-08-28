c4a7eebdf7a79acb39069c80ef0a665eb4d2e948
Louis <3874662+obelouix@users.noreply.github.com>
2021-08-29 01:41:23 +0200

    now auto equip the armor when clicked

--- src/main/java/fr/obelouix/essentials/gui/AdminToolsGUI.java
+++ src/main/java/fr/obelouix/essentials/gui/AdminToolsGUI.java
@@ -9,6 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -19,7 +20,7 @@ public class AdminToolsGUI extends BaseGUI {
 
     private @NotNull Inventory adminToolsInventory(Player player) {
         final Inventory inventory = Bukkit.createInventory(null, 54, Component.text(i18n.sendTranslatedMessage(player, "obelouix.gui.admin_tools")));
-        ItemMeta itemMeta = null;
+        final ItemMeta itemMeta = null;
 
         final ItemStack netherite_helmet = new ItemStack(Material.NETHERITE_HELMET, 1);
         final ItemStack netherite_chestplate = new ItemStack(Material.NETHERITE_CHESTPLATE, 1);
@@ -27,62 +28,47 @@ public class AdminToolsGUI extends BaseGUI {
         final ItemStack netherite_boots = new ItemStack(Material.NETHERITE_BOOTS, 1);
 
         // Protection X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
-        addEnchantement(netherite_chestplate, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
-        addEnchantement(netherite_leggings, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
-        addEnchantement(netherite_boots, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
+        addEnchantment(netherite_chestplate, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
+        addEnchantment(netherite_leggings, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
+        addEnchantment(netherite_boots, itemMeta, Enchantment.PROTECTION_ENVIRONMENTAL, 10);
 
         // Blast Protection X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
-        addEnchantement(netherite_chestplate, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
-        addEnchantement(netherite_leggings, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
-        addEnchantement(netherite_boots, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
+        addEnchantment(netherite_chestplate, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
+        addEnchantment(netherite_leggings, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
+        addEnchantment(netherite_boots, itemMeta, Enchantment.PROTECTION_EXPLOSIONS, 10);
 
         // Fire Protection X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.PROTECTION_FIRE, 10);
-        addEnchantement(netherite_chestplate, itemMeta, Enchantment.PROTECTION_FIRE, 10);
-        addEnchantement(netherite_leggings, itemMeta, Enchantment.PROTECTION_FIRE, 10);
-        addEnchantement(netherite_boots, itemMeta, Enchantment.PROTECTION_FIRE, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.PROTECTION_FIRE, 10);
+        addEnchantment(netherite_chestplate, itemMeta, Enchantment.PROTECTION_FIRE, 10);
+        addEnchantment(netherite_leggings, itemMeta, Enchantment.PROTECTION_FIRE, 10);
+        addEnchantment(netherite_boots, itemMeta, Enchantment.PROTECTION_FIRE, 10);
 
         // Projectile Protection X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
-        addEnchantement(netherite_chestplate, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
-        addEnchantement(netherite_leggings, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
-        addEnchantement(netherite_boots, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
+        addEnchantment(netherite_chestplate, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
+        addEnchantment(netherite_leggings, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
+        addEnchantment(netherite_boots, itemMeta, Enchantment.PROTECTION_PROJECTILE, 10);
 
         // Thorns X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.THORNS, 10);
-        addEnchantement(netherite_chestplate, itemMeta, Enchantment.THORNS, 10);
-        addEnchantement(netherite_leggings, itemMeta, Enchantment.THORNS, 10);
-        addEnchantement(netherite_boots, itemMeta, Enchantment.THORNS, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.THORNS, 10);
+        addEnchantment(netherite_chestplate, itemMeta, Enchantment.THORNS, 10);
+        addEnchantment(netherite_leggings, itemMeta, Enchantment.THORNS, 10);
+        addEnchantment(netherite_boots, itemMeta, Enchantment.THORNS, 10);
 
         // Respiration X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.OXYGEN, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.OXYGEN, 10);
+
+        addEnchantment(netherite_boots, itemMeta, Enchantment.PROTECTION_FALL, 10);
 
         // Aqua Affinity X
-        addEnchantement(netherite_helmet, itemMeta, Enchantment.WATER_WORKER, 10);
+        addEnchantment(netherite_helmet, itemMeta, Enchantment.WATER_WORKER, 10);
 
         inventory.setItem(0, createAdminArmor(netherite_helmet, "head"));
         inventory.setItem(9, createAdminArmor(netherite_chestplate, "chest"));
         inventory.setItem(18, createAdminArmor(netherite_leggings, "legs"));
         inventory.setItem(27, createAdminArmor(netherite_boots, "feet"));
-/*        ItemStack NETHERITE_CHESTPLATE = new ItemStack(Material.NETHERITE_CHESTPLATE, 1);
-        nbti = new NBTItem(NETHERITE_CHESTPLATE);
-        attribute = nbti.getCompoundList("AttributeModifiers");
-        mod1 = ((NBTCompoundList) attribute).addCompound();
-        mod1.setDouble("Amount", 1E6);
-        mod1.setString("AttributeName", "generic.knockback_resistance");
-        mod1.setString("Name", "generic.knockback_resistance");
-        mod1.setInteger("Operation", 0);
-        mod1.setInteger("UUIDLeast", -18788761);
-        mod1.setInteger("UUIDMost", 718533190);
-        mod1.setString("Slot", "head");
-        NETHERITE_CHESTPLATE = nbti.getItem();
-        itemMeta = NETHERITE_CHESTPLATE.getItemMeta();
-        itemMeta.setUnbreakable(true);
-        itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10, true);
-        NETHERITE_CHESTPLATE.setItemMeta(itemMeta);
-        inventory.setItem(8, NETHERITE_CHESTPLATE);*/
 
         return inventory;
     }
@@ -93,21 +79,58 @@ public class AdminToolsGUI extends BaseGUI {
     }
 
     @Override
+    @EventHandler
     public void cancelClick(InventoryClickEvent event) {
-
+        final Player player = (Player) event.getWhoClicked();
+        if (event.getView().title().equals(Component.text(i18n.sendTranslatedMessage(player, "obelouix.gui.admin_tools")))) {
+            event.setCancelled(true);
+        }
     }
 
     @Override
+    @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
+        final Player player = (Player) event.getWhoClicked();
+        if (event.getView().title().equals(Component.text(i18n.sendTranslatedMessage(player, "obelouix.gui.admin_tools")))) {
+            if (event.getSlot() == 0) {
 
+                if (player.getInventory().getHelmet() != null) {
+                    player.getInventory().addItem(player.getInventory().getHelmet());
+                }
+
+                player.getInventory().setHelmet(event.getCurrentItem());
+
+            } else if (event.getSlot() == 9) {
+
+                if (player.getInventory().getChestplate() != null) {
+                    player.getInventory().addItem(player.getInventory().getChestplate());
+                }
+
+                player.getInventory().setChestplate(event.getCurrentItem());
+
+            } else if (event.getSlot() == 18) {
+
+                if (player.getInventory().getLeggings() != null) {
+                    player.getInventory().addItem(player.getInventory().getLeggings());
+                }
+
+                player.getInventory().setLeggings(event.getCurrentItem());
+
+            } else if (event.getSlot() == 27) {
+
+                if (player.getInventory().getBoots() != null) {
+                    player.getInventory().addItem(player.getInventory().getBoots());
+                }
+
+                player.getInventory().setBoots(event.getCurrentItem());
+            }
+        }
     }
 
     private ItemStack createAdminArmor(ItemStack item, String slot) {
 
-//        ItemStack NETHERITE_HELMET = new ItemStack(Material.NETHERITE_HELMET, 1);
-        NBTItem nbtItem = new NBTItem(item);
-        NBTList attribute = nbtItem.getCompoundList("AttributeModifiers");
-//        NBTListCompound mod = ((NBTCompoundList) attribute).addCompound();
+        final NBTItem nbtItem = new NBTItem(item);
+        final NBTList<NBTListCompound> attribute = nbtItem.getCompoundList("AttributeModifiers");
         addAttribute(attribute, "generic.armor", 30, slot);
         addAttribute(attribute, "generic.knockback_resistance", 5, slot);
         addAttribute(attribute, "generic.armor_toughness", 20, slot);
@@ -118,8 +141,8 @@ public class AdminToolsGUI extends BaseGUI {
         return item;
     }
 
-    private void addAttribute(NBTList attributeList, String attribute, double amount, String slot) {
-        NBTListCompound mod = ((NBTCompoundList) attributeList).addCompound();
+    private void addAttribute(NBTList<NBTListCompound> attributeList, String attribute, double amount, String slot) {
+        final NBTListCompound mod = ((NBTCompoundList) attributeList).addCompound();
         mod.setDouble("Amount", amount);
         mod.setString("AttributeName", attribute);
         mod.setString("Name", attribute);
@@ -129,7 +152,7 @@ public class AdminToolsGUI extends BaseGUI {
         mod.setString("Slot", slot);
     }
 
-    private void addEnchantement(ItemStack item, ItemMeta itemMeta, Enchantment enchantment, int level) {
+    private void addEnchantment(ItemStack item, ItemMeta itemMeta, Enchantment enchantment, int level) {
         itemMeta = item.getItemMeta();
         itemMeta.setUnbreakable(true);
         itemMeta.addEnchant(enchantment, level, true);
