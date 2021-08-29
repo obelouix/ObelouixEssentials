5826c4f410c00398dd298bf04a1d1d0075c65e82
Louis <3874662+obelouix@users.noreply.github.com>
2021-08-29 19:03:08 +0200

    added javadoc

--- src/main/java/fr/obelouix/essentials/nbt/NbtWrapper.java
+++ src/main/java/fr/obelouix/essentials/nbt/NbtWrapper.java
@@ -4,6 +4,7 @@ import de.tr7zw.nbtapi.NBTCompoundList;
 import de.tr7zw.nbtapi.NBTItem;
 import de.tr7zw.nbtapi.NBTList;
 import de.tr7zw.nbtapi.NBTListCompound;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class NbtWrapper {
@@ -12,15 +13,42 @@ public class NbtWrapper {
         return new NBTItem(item);
     }
 
+    /**
+     * add a NBT Attribute and also set a slot like {@code "mainhand"} where this tag apply <br/>
+     * for example the slot {@code "mainhand} tell the game that this tag apply only when a {@link Player} has the item
+     * in his main hand
+     *
+     * @param attributeList {@linkplain NBTList} of {@link NBTListCompound} attributes
+     * @param attribute     a {@link String} like {@code generic.armor}
+     * @param amount        amount a {@link Double} like {@code 1.5} that represent the amount to apply on the given attribute
+     * @param slot          the slot where this tag apply
+     * @see <a href="https://minecraft.fandom.com/wiki/Attribute#Attributes">Attribute list</a>
+     */
     public static void addAttribute(NBTList<NBTListCompound> attributeList, String attribute, double amount, String slot) {
         final NBTListCompound mod = NBTCompoundList((NBTCompoundList) attributeList, attribute, amount);
         mod.setString("Slot", slot);
     }
 
+    /**
+     * add a NBT Attribute without a slot <br>
+     * if you want to add a slot see {@link #addAttribute(NBTList attributeList, String attribute, double amount, String slot)}
+     *
+     * @param attributeList {@linkplain NBTList} of {@link NBTListCompound} attributes
+     * @param attribute     a {@link String} like {@code generic.armor}
+     * @param amount        amount a {@link Double} like {@code 1.5} that represent the amount to apply on the given attribute
+     * @see <a href="https://minecraft.fandom.com/wiki/Attribute#Attributes">Attribute list</a>
+     */
     public static void addAttribute(NBTList<NBTListCompound> attributeList, String attribute, double amount) {
         NBTCompoundList((NBTCompoundList) attributeList, attribute, amount);
     }
 
+    /**
+     * @param attributeList {@linkplain NBTList} of {@link NBTListCompound} attributes
+     * @param attribute     a {@link String} like {@code generic.armor}
+     * @param amount        a {@link Double} like {@code 1.5} that represent the amount to apply on the given attribute
+     * @return {@link NBTListCompound}
+     * @see <a href="https://minecraft.fandom.com/wiki/Attribute#Attributes">Attribute list</a>
+     */
     private static NBTListCompound NBTCompoundList(NBTCompoundList attributeList, String attribute, double amount) {
         final NBTListCompound mod = attributeList.addCompound();
         mod.setDouble("Amount", amount);
