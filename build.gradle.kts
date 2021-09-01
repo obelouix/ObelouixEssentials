a8e66f026a0cf0a1dbc444b9a2659d126465f973
Louis <3874662+obelouix@users.noreply.github.com>
2021-09-01 13:44:58 +0200

    added version automation

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,15 @@ plugins {
     java
     id("xyz.jpenilla.run-paper") version "1.0.4"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.palantir.git-version") version "0.12.3"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.0"
 }
 
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+val details = versionDetails()
+
 group = "fr.obelouix"
-version = "1.0-SNAPSHOT"
+version = "0.2 - " + details.branchName + "-" + details.gitHash
 
 java {
     sourceCompatibility = JavaVersion.VERSION_16
