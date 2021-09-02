85fd22c4806591152ca2884637f225cf45731c0f
Louis <3874662+obelouix@users.noreply.github.com>
2021-09-02 19:28:47 +0200

    added permission to use color codes

--- src/main/java/fr/obelouix/essentials/event/ChatEvent.java
+++ src/main/java/fr/obelouix/essentials/event/ChatEvent.java
@@ -1,6 +1,8 @@
 package fr.obelouix.essentials.event;
 
+import fr.obelouix.essentials.Essentials;
 import fr.obelouix.essentials.config.Config;
+import fr.obelouix.essentials.permissions.IPermission;
 import fr.obelouix.essentials.utils.IPlayer;
 import fr.obelouix.essentials.utils.TextColorFormatter;
 import io.papermc.paper.event.player.AsyncChatEvent;
@@ -14,7 +16,11 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Pattern;
+
 public class ChatEvent implements Listener {
+
+    private final Essentials plugin = Essentials.getInstance();
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void formatChat(AsyncChatEvent event) {
@@ -23,17 +29,30 @@ public class ChatEvent implements Listener {
     }
 
     private @NotNull Component chatRender(@NotNull Player player, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience audience) {
-        final @NotNull TextComponent prefix = Component.text(IPlayer.getPrefix(player));
-        final @NotNull TextComponent suffix = Component.text(IPlayer.getSuffix(player));
-        final @NotNull Component chatFormat = Component.text(Config.chatFormat.get(IPlayer.getGroup(player)));
-        final @NotNull String serializedFormat = PlainTextComponentSerializer.plainText().serialize(chatFormat)
-                .replaceFirst("\\{world}", player.getWorld().getName())
-                .replaceFirst("\\{prefix}", PlainTextComponentSerializer.plainText().serialize(prefix))
-                .replaceFirst("\\{displayname}", player.getName())
-                .replaceFirst("\\{suffix}", PlainTextComponentSerializer.plainText().serialize(suffix))
-                .replaceFirst("\\{message}", PlainTextComponentSerializer.plainText().serialize(message));
+        Component msg = message;
 
-        return TextColorFormatter.colorFormatter(PlainTextComponentSerializer.plainText().deserialize(serializedFormat));
+        // If the message sender doesn't have the permission to format his message, remove all formatting codes
+        if (!IPermission.simpleTest(player, "obelouix.chat.formatting")) {
+            msg = msg.replaceText(builder -> builder.match(Pattern.compile("(&)?&([0-9a-fk-orA-FK-OR])"))
+                            .replacement(""))
+                    .replaceText(builder -> builder.match(Pattern.compile("(&)?&#([0-9a-fA-F]{6})")).replacement(""));
+        }
+        if (plugin.getLuckPermsAPI() != null) {
+            final @NotNull TextComponent prefix = Component.text(IPlayer.getPrefix(player));
+            final @NotNull TextComponent suffix = Component.text(IPlayer.getSuffix(player));
+            final @NotNull Component chatFormat = Component.text(Config.chatFormat.get(IPlayer.getGroup(player)));
+
+
+            final @NotNull String serializedFormat = PlainTextComponentSerializer.plainText().serialize(chatFormat)
+                    .replaceFirst("\\{world}", player.getWorld().getName())
+                    .replaceFirst("\\{prefix}", PlainTextComponentSerializer.plainText().serialize(prefix))
+                    .replaceFirst("\\{displayname}", player.getName())
+                    .replaceFirst("\\{suffix}", PlainTextComponentSerializer.plainText().serialize(suffix))
+                    .replaceFirst("\\{message}", PlainTextComponentSerializer.plainText().serialize(msg));
+
+            return TextColorFormatter.colorFormatter(PlainTextComponentSerializer.plainText().deserialize(serializedFormat));
+        }
+        return sourceDisplayName.append(Component.text(": ")).append(msg);
     }
 
 }
